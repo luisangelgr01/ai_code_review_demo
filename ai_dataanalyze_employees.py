fd34b94dd05bc44d127e224e759c614d9e55867e
Luis Ángel González Romo <luis.gonzalez@definityfirst.com>
2024-10-07 10:44:46 -0600

    exmple changes to python example code

--- ai_dataanalyze_employees.py
+++ ai_dataanalyze_employees.py
@@ -34,11 +34,11 @@ print(get_employees())
 employees_response = pd.DataFrame(get_employees()) #Convert response to dataframe
 
 api_type = "azure"
-api_base = os.getenv("AZURE_OPENAI_ENDPOINT_1")
-api_version = os.getenv("API_VERSION_1")
-api_key = os.getenv("AZURE_OPENAI_API_KEY_1")
-deployment_name = os.getenv("DEPLOYMENT_NAME_1")
-model_name = os.getenv("MODEL_NAME_1")
+api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+api_version = os.getenv("API_VERSION")
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+deployment_name = os.getenv("DEPLOYMENT_NAME")
+model_name = os.getenv("MODEL_NAME")
 
 print(api_type)
 print(api_base)
@@ -56,6 +56,6 @@ llm = AzureOpenAI(
 
 
 agent = create_pandas_dataframe_agent(llm, employees_response, verbose=True, allow_dangerous_code=True)
-#agent.run("What is the amount of employees whose end date is between '2024-01-01' and 2024-06-30'?")
-agent.run("What is the amount of employees whose end date is null?")
+agent.run("What is the amount of employees whose end date is between '2024-01-01' and 2024-06-30'?")
+#agent.run("What is the amount of employees whose end date is null?")
 #agent.run("Which teams exist?")
